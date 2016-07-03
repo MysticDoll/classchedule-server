75fda5cb40b53bdf44bd73917d817dbe30b17137
Mitsuru Takigahira <mysticdoll@mysticdoll.com>
2016-07-03 16:25:59 +0900

    Add days and periods in schedule table

--- devel/vm/vueTemplate.js
+++ devel/vm/vueTemplate.js
@@ -1,7 +1,30 @@
 module.exports = 
 `
 <section class="schedules">
-  <div v-for="period in periods" class="schedule-row">
+  <div class="schedule-row">
+    <div class="schedule-cell schedule-days">
+      <h4>月</h4>
+    </div>
+    <div class="schedule-cell schedule-days">
+      <h4>火</h4>
+    </div>
+    <div class="schedule-cell schedule-days">
+      <h4>水</h4>
+    </div>
+    <div class="schedule-cell schedule-days">
+      <h4>木</h4>
+    </div>
+    <div class="schedule-cell schedule-days">
+      <h4>金</h4>
+    </div>
+    <div class="schedule-cell schedule-days">
+      <h4>土</h4>
+    </div>
+  </div>
+  <div v-for="(index, period) in periods" class="schedule-row">
+    <div class="schedule-cell schedule-period">
+      <h4>{{index + 1}}</h4>
+    </div>
     <div v-for="schedule in period" class="schedule-cell">
       <div class="schedule-title">
         <h4>講義名</h4>
@@ -13,7 +36,7 @@ module.exports =
       </div>
     </div>
   </div>
-  <button id="save" v-on:click="save()">Save</button>
-  <button id="export" v-on:click="exportiCal()">export</button>
+  <button id="save" v-on:click="save()">保存</button>
+  <button id="export" v-on:click="exportiCal()">ダウンロード</button>
 </section> 
 `;
